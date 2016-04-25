e916145a3ff22c4f0a0f497279216ae68fe43146
Alastair Porter <alastair@porter.net.nz>
2016-04-25 12:59:44 +0200

    retry failed web queries and cache intermediate results

--- mbfingerprint.py
+++ mbfingerprint.py
@@ -2,23 +2,52 @@ import os
 import argparse
 import acoustid
 import config
+import time
+import json
+
+RETRIES = 5
 
 def fingerprint(thefile):
-    for score, recording_id, title, artist in acoustid.match(config.ACOUSTID_KEY, thefile):
-        #print("{} - {} - {}".format(artist, title, recording_id))
-        return 1
-    return 0
+    attempt = 0
+    success = False
+    while not success and attempt < RETRIES:
+        try:
+            matches = acoustid.match(config.ACOUSTID_KEY, thefile)
+            success = True
+        except acoustid.WebServiceError as e:
+            time.sleep(1)
+            attempt += 1
+
+    if success:
+        return list(matches)
+    return []
+
+def save_cache(data):
+    json.dump(data, open("lookupcache.json", "w"))
 
 def main(thedir):
+    if os.path.exists("lookupcache.json"):
+        data = json.load(open("lookupcache.json"))
+    else:
+        data = {}
     total = 0
     matches = 0
     for root, dirs, files in os.walk(thedir):
         for f in files:
             fname = os.path.join(root, f)
             print(fname)
-            fp = fingerprint(fname)
-            matches += fp
+            if fname not in data:
+                matchdata = fingerprint(fname)
+                data[fname] = matchdata
+            else:
+                matchdata = data[fname]
+            if matchdata:
+                matches += 1
             total += 1
+            if total % 10 == 0:
+                print("{}: matched {}/{} fingerprints".format(total, matches, total))
+                save_cache(data)
+    save_cache(data)
     print("matched {}/{} files with fingerprint".format(matches, total))
 
 if __name__ == "__main__":
